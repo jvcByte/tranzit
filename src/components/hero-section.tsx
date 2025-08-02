91de6b382a467a5b4465c79fc08c216e9776b9b1
jvcbyte <jvc8463@gmail.com>
2025-08-02 21:30:06 +0100

    Refactor: change hero-section image

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -49,8 +49,8 @@ export default function HeroSection() {
                                 muted
                                 playsInline
                                 className="size-full object-cover opacity-50 invert dark:opacity-35 dark:invert-0 dark:lg:opacity-75"
-                                // src="https://ik.imagekit.io/lrigu76hy/tailark/dna-video.mp4?updatedAt=1745736251477"></video>
-                                src="/bg1.webm"></video>
+                                src="https://ik.imagekit.io/lrigu76hy/tailark/dna-video.mp4?updatedAt=1745736251477"></video>
+                                {/* // src="/bg1.webm"></video> */}
                         </div>
                     </div>
                 </section>
