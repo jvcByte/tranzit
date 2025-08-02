b9f03a0ff72415ec6b43f1bd6cad051b0341ce54
jvcbyte <jvc8463@gmail.com>
2025-08-02 01:42:24 +0100

    Update: add mp4 of cars

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -49,7 +49,8 @@ export default function HeroSection() {
                                 muted
                                 playsInline
                                 className="size-full object-cover opacity-50 invert dark:opacity-35 dark:invert-0 dark:lg:opacity-75"
-                                src="https://ik.imagekit.io/lrigu76hy/tailark/dna-video.mp4?updatedAt=1745736251477"></video>
+                                // src="https://ik.imagekit.io/lrigu76hy/tailark/dna-video.mp4?updatedAt=1745736251477"></video>
+                                src="/bg1.mp4"></video>
                         </div>
                     </div>
                 </section>
