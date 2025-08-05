c20c7799fee85ada8c90a3d62489b79d2830f0cf
jvcbyte <jvc8463@gmail.com>
2025-08-05 22:21:21 +0100

    Update: add heade and footer to login and signup page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Login from "@/components/forms/login-form";
+import { HeroHeader } from "@/components/layout/header";
+import FooterSection from "@/components/layout/footer-section";
 
 export default function LoginPage() {
     return (
         <>
+            <HeroHeader />
             <Login />
+            <FooterSection />
         </>
     );
 }
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Signin from "@/components/forms/signup-form";
+import { HeroHeader } from "@/components/layout/header";
+import FooterSection from "@/components/layout/footer-section";
 
 export default function SignupPage() {
   return (
     <>
+      <HeroHeader />
       <Signin />
+      <FooterSection />
     </>
   );
 }
