e39f3eeea0bd239328e7f2c3e19efb78e053e3f2
jvcbyte <jvc8463@gmail.com>
2025-08-31 23:01:45 +0100

    reafcator: remove webpack, add turbopack

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   turbopack: {},
-  // Configure external packages for server components
+  
   serverExternalPackages: ['pg'],
 };
 
