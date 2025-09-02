20b1dcbbfe16324893a5aa0668931dfb493cfcce
jvcbyte <jvc8463@gmail.com>
2025-09-03 00:31:00 +0100

    modify header

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -9,10 +9,10 @@ import { Button } from '../ui/button'
 // import { Button } from '../ui/button'
 
 const menuItems = [
-    { name: 'How it works', href: '/how-it-works' },
-    { name: 'Choose us', href: '/choose-us' },
-    { name: 'For partners', href: '/for-partners' },
-    { name: 'For affiliates', href: '/for-affiliates' },
+    { name: 'Ride', href: '/how-it-works' },
+    { name: 'Drive', href: '/choose-us' },
+    { name: 'Business', href: '/for-partners' },
+    { name: 'Affiliates', href: '/for-affiliates' },
 ]
 
 export const HeroHeader = () => {
@@ -35,15 +35,37 @@ export const HeroHeader = () => {
                 className={cn('fixed z-20 w-full border-b transition-colors duration-150 bg-background', scrolled)}>
                 <div className="m-auto max-w-[1660px] px-6">
                     <div className="flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4">
-                        <div className="flex w-full justify-between lg:w-auto">
+                        <div className="flex w-full items-center justify-between lg:w-auto gap-5">
                             <Link
                                 href="/"
                                 aria-label="home"
                                 className="flex items-center space-x-2">
-                                <p className='text-[45px] font-700 font-bold text-[#99da3d]'>Tranzit</p>
+                                <p className='md:text-[45px] text-xl font-700 font-bold text-[#99da3d]'>Tranzit</p>
                             </Link>
 
-                            <div className='flex items-center space-x-2 gap-4'>
+                            
+
+                            <div className='flex items-center gap-2'>
+                                <div className="flex w-full flex-row gap-2 lg:hidden">
+                                    <Button
+                                        asChild
+                                        // variant="outline"
+                                        size="sm"
+                                        className="border-0 bg-transparent text-"
+                                    >
+                                        <Link href="/login">
+                                            <span>Login</span>
+                                        </Link>
+                                    </Button>
+                                    <Button
+                                        asChild
+                                        size="sm"
+                                    >
+                                        <Link href="/signup">
+                                            <span>Sign Up</span>
+                                        </Link>
+                                    </Button>
+                                </div>
                                 <button
                                     onClick={() => setMenuState(!menuState)}
                                     aria-label={menuState == true ? 'Close Menu' : 'Open Menu'}
@@ -51,14 +73,25 @@ export const HeroHeader = () => {
                                     <Menu className="in-data-[state=active]:rotate-180 in-data-[state=active]:scale-0 in-data-[state=active]:opacity-0 m-auto size-6 duration-200" />
                                     <X className="in-data-[state=active]:rotate-0 in-data-[state=active]:scale-100 in-data-[state=active]:opacity-100 absolute inset-0 m-auto size-6 -rotate-180 scale-0 opacity-0 duration-200" />
                                 </button>
-                                <div className='lg:hidden'>
-                                    <ThemeSwitch />
-                                </div>
+                            </div>
+
+                            <div className="hidden lg:block">
+                                <ul className="text-base lg:flex lg:gap-2 lg:text-sm">
+                                    {menuItems.map((item, index) => (
+                                        <li key={index}>
+                                            <Link
+                                                href={item.href}
+                                                className="text-lg font-medium hover:bg-primary/20 hover:rounded px-3 block duration-150">
+                                                <span>{item.name}</span>
+                                            </Link>
+                                        </li>
+                                    ))}
+                                </ul>
                             </div>
                         </div>
 
                         <div className="bg-background in-data-[state=active]:block lg:in-data-[state=active]:flex mb-6 hidden w-full flex-wrap items-center justify-end space-y-8 rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none dark:shadow-none dark:lg:bg-transparent">
-                            <div className="lg:pr-4">
+                            <div className="lg:hidden">
                                 <ul className="space-y-6 text-base lg:flex lg:gap-8 lg:space-y-0 lg:text-sm">
                                     {menuItems.map((item, index) => (
                                         <li key={index}>
@@ -71,7 +104,7 @@ export const HeroHeader = () => {
                                     ))}
                                 </ul>
                             </div>
-                            <div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit">
+                            <div className="flex w-full flex-col space-y-3 hidden md:inline-flex sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit">
                                 <Button
                                     asChild
                                     variant="outline"
@@ -84,11 +117,11 @@ export const HeroHeader = () => {
                                     asChild
                                     size="sm">
                                     <Link href="/signup">
-                                        <span>Sign Up</span>
+                                        <span className='text-black'>Sign Up</span>
                                     </Link>
                                 </Button>
                             </div>
-                            <div className='hidden lg:inline-flex'>
+                            <div className='text-black bg-primary rounded-md p-0 flex items-center justify-center'>
                                 <ThemeSwitch />
                             </div>
                         </div>
